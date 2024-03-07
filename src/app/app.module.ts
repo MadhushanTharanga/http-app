6ed10b44401cd5bf6f1adac254bcf1c997428970
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-03-07 10:05:48 +0530

    added angular meterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllComponent } from './components/all/all.component';
 import { NewComponent } from './components/new/new.component';
-import { FineComponent } from './components/fine/fine.component';
+// import { FineComponent } from './components/fine/fine.component';
 import { UpdateComponent } from './components/update/update.component';
 import { DeleteComponent } from './components/delete/delete.component';
 import { FindComponent } from './components/find/find.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     AllComponent,
     NewComponent,
-    FineComponent,
+    // FineComponent,
     UpdateComponent,
     DeleteComponent,
     FindComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    Meter
   ],
   providers: [],
   bootstrap: [AppComponent]
