22d0bc2b7266685b2491605fa4fb7d825c4d3384
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-03-07 08:50:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AllComponent } from './components/all/all.component';
+import { NewComponent } from './components/new/new.component';
+import { FineComponent } from './components/fine/fine.component';
+import { UpdateComponent } from './components/update/update.component';
+import { DeleteComponent } from './components/delete/delete.component';
+import { FindComponent } from './components/find/find.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllComponent,
+    NewComponent,
+    FineComponent,
+    UpdateComponent,
+    DeleteComponent,
+    FindComponent
   ],
   imports: [
     BrowserModule,
