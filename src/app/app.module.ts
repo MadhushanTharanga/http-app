9b6aff30c51393ebcb8722327ce95e350bf0afb6
Madhushan Tharanga <110463634+MadhushanTharanga@users.noreply.github.com>
2024-03-17 05:36:51 +0530

    get data on table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {MatInputModule} from "@angular/material/input";
     MatButtonModule,
     HttpClientModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule
 
   ],
   providers: [],
--- src/app/components/find/find.component.ts
+++ src/app/components/find/find.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-find',
   templateUrl: './find.component.html',
   styleUrls: ['./find.component.scss']
 })
-export class FindComponent implements OnInit {
+export class FindComponent  {
 
-  constructor() { }
+  searchId='';
+  list: Array<any>=[];
+  constructor(private http:HttpClient) { }
 
-  ngOnInit(): void {
+
+  loadData(){
+    this.http.get<any>('https://jsonplaceholder.typicode.com/posts?id='+this.searchId)
+      .subscribe(response=>{
+        console.log(response);
+        this.list=response;
+        console.log(this.list)
+      });
   }
-
 }
